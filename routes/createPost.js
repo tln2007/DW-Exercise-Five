cc7cad77f47d30cfb2538e38583f02ab15922193
Tasnim Nahar <tln2007@nyu.edu>
2022-11-10 08:47:06 -0500

    add form to firebase

--- routes/createPost.js
+++ routes/createPost.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const firestore = require('firebase/firestore');
+const db = firestore.getFirestore();
 
 const createPostForm = `
 <h1>Create Post</h1>
-<form action="">
-    <input type="text" name="title" placeholder="Title" />
-    <input type="text" name="text" placeholder="Text" />
-    <input type="text" name="author" placeholder="Author" />
-    <button type="submit">Submit Button</button>
+<form action="/create/submit">
+    <div style="display: flex; flex-direction: column; max-width: 400px;">
+        <label for="title">Title</label>
+        <input type="text" name="postTitle" placeholder="Title" style="margin-bottom: 20px;" />
+        <label for="text">Text</label>
+        <input type="text" name="postText" placeholder="Text" style="margin-bottom: 20px;" />
+        <label for="author">Author</label>
+        <input type="author" name="postAuthor" placeholder="Author" style="margin-bottom: 20px;" />
+        <button type="submit">Submit Button</button>
+    </div>
 </form>
 `;
 
@@ -17,6 +24,35 @@ router.use((req, res, next) => {
 
 router.get('/', (req, res) => {
     res.send(createPostForm);
+});
+
+router.get('/submit', (req, res) => {
+    // using values from form input
+    const queryParams = req.query
+    const title = queryParams.postTitle;
+    const text = queryParams.postText;
+    const author = queryParams.postAuthor;
+    const idFromTitle = title.replace(/\s+/g, "-").toLowerCase();
+    //submit post to firebase
+    const setBlogPost = firestore.setDoc(
+        firestore.doc(db, "posts", idFromTitle), {
+            title: title,
+            text: text,
+            author: author,
+    });
+
+    setBlogPost
+        .then((response) => {
+            res.send(`
+                <h1>Submission successful!</h1>
+                <p><a href="/create">Add Another Post</a></p>
+                <p><a href="/">Return Home</a></p>
+            `); 
+        })
+        .catch((error) => {
+            console.warn(error);
+            res.send(`Error Submitting: ${error.toString()}`);
+        });
 });
 
 module.exports = router;
